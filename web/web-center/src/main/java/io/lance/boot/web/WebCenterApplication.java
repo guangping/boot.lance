2b068657aa8b3cecf428feb2c059a18b58e38f05
mgp <mgp@ebidding.com.cn>
2017-09-20 16:35:52 +0800

    添加服务

---
+++ web/web-center/src/main/java/io/lance/boot/web/WebCenterApplication.java
@@ -0,0 +1,15 @@
+package io.lance.boot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class WebCenterApplication {
+
+	public static void main(String[] args) {
+		System.out.println("注册中心 start ......");
+		SpringApplication.run(WebCenterApplication.class, args);
+	}
+}
--- web/web-ui/src/main/java/io/lance/boot/web/AppApplication.java
+++ web/web-ui/src/main/java/io/lance/boot/web/AppApplication.java
@@ -2,8 +2,10 @@ package io.lance.boot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableDiscoveryClient
 @EnableAsync
 @SpringBootApplication(scanBasePackages = {"io.lance.boot.common.*","io.lance.boot.web.*"})
 public class AppApplication {
