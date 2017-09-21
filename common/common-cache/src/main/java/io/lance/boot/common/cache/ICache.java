ead08b3359158827138023b4333e58bf30995fe4
mgp <mgp@ebidding.com.cn>
2017-09-21 17:19:02 +0800

    添加缓存部分支持

---
+++ common/common-cache/src/main/java/io/lance/boot/common/cache/ICache.java
@@ -0,0 +1,44 @@
+package io.lance.boot.common.cache;
+
+/**
+ * @desc: 缓存规范接口
+ * @author: lance
+ * @time: 2017-09-21 17:13
+ */
+public interface ICache {
+
+    void set(String key, String value);
+
+    void set(String key, String value, int expire);
+
+    void get(String key);
+
+    void del(String key);
+
+    /**
+     * @desc: 增长
+     * @author: lance
+     * @time: 2017-09-21 17:15:36
+     */
+    long incr(String key, long by);
+
+    long incr(String key, long by, long def);
+
+    /**
+     * @desc: 获取对象
+     * @author: lance
+     * @time: 2017-09-21 17:16:24
+     */
+    <T> T getObj(String key, Class<T> clazz);
+
+    /**
+     * @desc: 设置对象
+     * @author: lance
+     * @time: 2017-09-21 17:17:36
+     */
+    void set(String key, Object value);
+
+    void set(String key, Object value, int expire);
+
+
+}
